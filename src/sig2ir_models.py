024c4850cd4ebe33eca8704f798b624def13b946
Joanna Luberadzka <joa.luberadzka@gmail.com>
2023-08-16 23:48:28 +0200

    progressed with the decoder, now debugging all dimensions, I need to understand the dimensions in the film operation, there were some bugs there

--- src/sig2ir_models.py
+++ src/sig2ir_models.py
@@ -14,8 +14,9 @@ import helpers
 # -------------------------------------------- ENCODER: --------------------------------------------
 # --------------------------------------------------------------------------------------------------
 
-# -------- Definition of a convolutional block used in the encoder: --------
+
 class EncBlock(nn.Module):
+    # -------- One encoder block: --------
     def __init__(self, in_channels, out_channels, kernel_size=15, stride=2, padding=0):
         super().__init__()
         # direct connection
@@ -38,9 +39,10 @@ class EncBlock(nn.Module):
         x += residual
         return x
     
-# -------- Definition of the encoder: --------
-class sig2ir_decoder(nn.Module):
-    def __init__(self, x_len=16000*3, z_len=512, N_layers=9):
+
+class sig2ir_encoder(nn.Module):
+    # -------- Encoder: --------
+    def __init__(self, x_len=16000*3, l_len=512, N_layers=9):
         super().__init__() 
 
         # internal parameters of the network:
@@ -66,7 +68,7 @@ class sig2ir_decoder(nn.Module):
         # final mlp layers
         self.mlp = nn.Sequential(nn.Linear(block_channels, block_channels),
                                  nn.Linear(block_channels,int(block_channels/2)),
-                                 nn.Linear(int(block_channels/2),z_len))
+                                 nn.Linear(int(block_channels/2),l_len))
         
     def forward(self, x):
         # Convolutional residual blocks:
@@ -82,94 +84,243 @@ class sig2ir_decoder(nn.Module):
 # -------------------------------------------- DECODER: --------------------------------------------
 # --------------------------------------------------------------------------------------------------
 
-# -------- Definition of a convolutional block used in the decoder: --------
-class GBlock(nn.Module):
-    # def __init__(self, in_channels, hidden_channels, z_channels, upsample_factor):
-    #     super().__init__()
-    #     # parameters of decoder block
-    #     self.in_channels = in_channels # this is the size of v 
-    #     self.hidden_channels = hidden_channels
-    #     self.z_channels = z_channels
-    #     self.upsample_factor = upsample_factor
-    #     # Decoder block, stage A 
-    #     self.A_direct_bn1=nn.BatchNorm1d(in_channels)
-    #     self.A_direct_film1=nn.FiLM(in_channels*2,in_channels)
-    #     self.A_direct_prelu1=nn.PReLU()
-    #     self.A_direct_upsmpl
-    #     self.A_direct_conv
-    #     self.A_direct_bn2=nn.BatchNorm1d()
-    #     self.A_direct_film2
-    #     self.A_direct_prelu2
-    #     self.A_residual_upsmpl
-    #     self.A_residual_conv
-    #     # Decoder block, stage B
-    #     self.B_direct_bn1=nn.BatchNorm1d()
-    #     self.B_direct_film1
-    #     self.B_direct_prelu1
-    #     self.B_direct_dilconv1
-    #     self.B_direct_bn2=nn.BatchNorm1d()
-    #     self.B_direct_film2
-    #     self.B_direct_prelu2
-    #     self.B_direct_dilconv2
+class DecBlock(nn.Module):
+    # -------- One decoder block: --------
+    def __init__(self,
+                 in_channels,
+                 hidden_channels,
+                 z_channels,
+                 upsample_factor):
+        super().__init__()
 
-    def noise_concat(self, z):
-        # concatenate noise and latent variable
-        # noise has the same dimension as the latent variable 
-        # n= generate noise
-        # condition = [n, z]
-        # return condition
-        return
-    
-    def forward(self, z, v):
-        
-        # v - single trainable vector (how to generate this?)
-        # z - latent variable
-        # inputs = condition
+        self.in_channels = in_channels
+        self.hidden_channels = hidden_channels
+        self.z_channels = z_channels
+        self.upsample_factor = upsample_factor
 
-        # outputs= self.A_direct_bn1=nn.BatchNorm1d(self.in_channels)
-        # outputs = self.condition_batchnorm1(inputs, z)
-        # outputs = self.first_stack(outputs)
-        # outputs = self.condition_batchnorm2(outputs, z)
-        # outputs = self.second_stack(outputs)
+        # Decoder block, stage A
+        self.A_direct_bn1=nn.BatchNorm1d(in_channels)
+        self.A_direct_film1=FiLM(z_channels,in_channels)
+        self.A_direct_prelu1=nn.PReLU()
+        self.A_direct_upsmpl=UpsampleNet(in_channels, in_channels, upsample_factor)
+        self.A_direct_conv=customConv1d(in_channels, hidden_channels, kernel_size=3)
+        self.A_direct_bn2=nn.BatchNorm1d(hidden_channels)
+        self.A_direct_film2=FiLM(z_channels,hidden_channels)
+        self.A_direct_prelu2=nn.PReLU()
 
-        # residual_outputs = self.residual1(inputs) + outputs
+        self.A_residual_upsmpl=UpsampleNet(in_channels, in_channels, upsample_factor)
+        self.A_residual_conv=customConv1d(in_channels, hidden_channels, kernel_size=1)
 
-        # outputs = self.condition_batchnorm3(residual_outputs, z)
-        # outputs = self.third_stack(outputs)
-        # outputs = self.condition_batchnorm4(outputs, z)
-        # outputs = self.fourth_stack(outputs)
+        # Decoder block, stage B
+        self.B_direct_bn1=nn.BatchNorm1d(hidden_channels)
+        self.B_direct_film1=FiLM(hidden_channels,z_channels)
+        self.B_direct_prelu1=nn.PReLU()
+        self.B_direct_dilconv1=customConv1d(hidden_channels, hidden_channels, kernel_size=3, dilation=4)
+        self.B_direct_bn2=nn.BatchNorm1d(hidden_channels)
+        self.B_direct_film2=FiLM(hidden_channels,z_channels)
+        self.B_direct_prelu2=nn.PReLU()
+        self.B_direct_dilconv2=customConv1d(hidden_channels, hidden_channels, kernel_size=3, dilation=8)
 
-        # outputs = outputs + residual_outputs
-        # return outputs
-        return
+    def concat_noise(self,z):
+        print(z.shape)
+        n=torch.randn(z.shape)
+        zn = torch.cat((z, n), dim=-1)
+        return zn
 
-# -------- Definition of a FiLM layer: --------
+    def forward(self, input, z):
+        # block A - direct connection
+        x=input
+        x=self.A_direct_bn1(x)
+        x=self.A_direct_film1(x,self.concat_noise(z))
+        x=self.A_direct_prelu1(x)
+        x=self.A_direct_upsmpl(x)
+        x=self.A_direct_conv(x)
+        x=self.A_direct_bn2(x)
+        x=self.A_direct_film2(x,self.concat_noise(z))
+        x=self.A_direct_prelu2(x)
+        # block A - residual connection
+        res=self.A_residual_upsmpl(input)
+        res=self.A_residual_conv(res)
+        # output of block A - sum of direct and residual
+        x_a = res + x
+        # block B - direct connection
+        x_b=self.B_direct_bn1(x_a)
+        x_b=self.B_direct_film1(x_b,self.concat_noise(z))
+        x_b=self.B_direct_prelu1(x_b)
+        x_b=self.B_direct_dilconv1(x_b)
+        x_b=self.B_direct_bn2(x_b)
+        x_b=self.B_direct_film2(x_b,self.concat_noise(z))
+        x_b=self.B_direct_prelu2(x_b)
+        x_b=self.B_direct_dilconv2(x_b)
+        # output of block A - sum of direct and residual
+        output = x_a + x_b
+
+        return output
+
+class sig2ir_decoder(nn.Module):
+    # -------- Decoder: --------
+    def __init__(self,
+                 in_channels=567,
+                 z_len=128):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.z_channels = z_len
+
+        self.preprocess = customConv1d(in_channels, 768, kernel_size=3)
+        self.gblocks = nn.ModuleList ([
+            DecBlock(768, 768, z_len, 1),
+            DecBlock(768, 768, z_len, 1),
+            DecBlock(768, 384, z_len, 2),
+            DecBlock(384, 384, z_len, 2),
+            DecBlock(384, 384, z_len, 2),
+            DecBlock(384, 192, z_len, 3),
+            DecBlock(192, 96, z_len, 5)
+        ])
+        self.postprocess = nn.Sequential(
+            customConv1d(96, 1, kernel_size=3),
+            nn.Tanh()
+        )
+
+    def forward(self, inputs, z):
+        inputs = self.preprocess(inputs)
+        outputs = inputs
+        for (i, layer) in enumerate(self.gblocks):
+            outputs = layer(outputs, z)
+        outputs = self.postprocess(outputs)
+        return outputs
+
+class UpsampleNet(nn.Module):
+    # ----- Upsampling with transposed 1d convolutions -----
+    def __init__(self,
+                 input_size,
+                 output_size,
+                 upsample_factor):
+
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.upsample_factor = upsample_factor
+
+        layer = nn.ConvTranspose1d(input_size, output_size, upsample_factor * 2,
+                                   upsample_factor, padding=upsample_factor // 2)
+        nn.init.orthogonal_(layer.weight)
+        self.layer = nn.utils.spectral_norm(layer)
+
+    def forward(self, inputs):
+        outputs = self.layer(inputs)
+        outputs = outputs[:, :, : inputs.size(-1) * self.upsample_factor]
+        return outputs
+
+class customConv1d(nn.Module):
+    # ---- Conv1d for spectral normalisation and orthogonal initialisation ----
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size=1,
+                 stride=1,
+                 dilation=1,
+                 groups=1):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.dilation = dilation
+        self.groups = groups
+        pad = dilation * (kernel_size - 1) // 2
+
+        layer = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size,
+                stride=stride, padding=pad, dilation=dilation, groups=groups)
+        nn.init.orthogonal_(layer.weight)
+        self.layer = nn.utils.spectral_norm(layer)
+
+    def forward(self, inputs):
+        return self.layer(inputs)
+
 class FiLM(nn.Module):
+    # -------- Definition of a FiLM layer: --------
     def __init__(self, zdim, n_channels):
         super().__init__()
         self.gamma = nn.Linear(zdim, n_channels)   
         self.beta = nn.Linear(zdim, n_channels)   
 
     def forward(self, x, z):
-        gamma = self.gamma(z).unsqueeze(-1).unsqueeze(-1)
-        beta = self.beta(z).unsqueeze(-1).unsqueeze(-1)
+        gamma = self.gamma(z).unsqueeze(-1)
+        beta = self.beta(z).unsqueeze(-1)
         x = gamma * x + beta
         return x
+    
+
+# --------------------------------------------------------------------------------------------------
+# ----------------------------------------- FULL NETWORK: ------------------------------------------
+# --------------------------------------------------------------------------------------------------
+
+class sig2ir_encdec(nn.Module):
+    # -------- Decoder: --------
+    def __init__(self, 
+                 sig_len=int(48000*2.4), 
+                 l_len=128, 
+                 v_len=400, 
+                 z_len=128*2, 
+                 ir_len=48000):
+        super().__init__()
+
+        self.sig_len=sig_len #input waveform
+        self.l_len=l_len #bottleneck embedding
+        self.v_len=v_len #input sequence to decoder 
+        self.z_len=z_len #conditioning vector
+        self.ir_len # output waveform
+
+        self.encode=sig2ir_encoder(x_len=sig_len, l_len=l_len, N_layers=9)
+        self.decode=sig2ir_decoder(in_channels=1,z_channels=z_len)
+        self.trainable_v = torch.nn.Parameter(torch.randn(v_len)) 
+
+    def forward(self,x):
+        l=self.encoder(x)
+        v=self.trainable_v
+        ir=self.decode(v,l)
+        return ir
+
 
 # check if the model definitions are correct
 if __name__ == "__main__":
     
     # example input tensor
     FS=48000
-    x_len=3*FS
-    x_wave=torch.randn(1,1,x_len)
+    sig_len=int(2.4*FS)
+    l_len=512
+    v_len=400 
+    z_len=512*2
+    ir_len=FS
+    
+    x_wave=torch.randn(1,1,sig_len)
 
-    # instantiate the model
-    encoder=sig2ir_decoder(x_len=x_len,z_len=128,N_layers=9)
+    # check encoder
+    encoder=sig2ir_encoder(x_len=sig_len,l_len=l_len,N_layers=9)
     encoder.to("cpu")
-    summary(encoder,input_size = (1, x_len), batch_size = -1, device="cpu")# torch summary expects 2 dim input for MLP
+    summary(encoder,input_size = (1, sig_len), batch_size = -1, device="cpu")# torch summary expects 2 dim input for 1d conv
     encoder.eval()
-    z=encoder(x_wave)
-    print(f"input shape: {x_wave.shape}")
-    print(f"output shape: {z.shape}")
+    l=encoder(x_wave)
+    print(f"encoder input shape: {x_wave.shape}")
+    print(f"encoder output shape: {l.shape}")
+
+    # check decoder
+    decoder=sig2ir_decoder(in_channels=1,z_len=z_len)
+    decoder.to("cpu")
+    decoder.eval()
+    v=torch.nn.Parameter(torch.randn(1,1,v_len)) 
+    y=decoder(v,l)
+
+    print(f"decoder input shape: {l.shape}")
+    print(f"output shape: {y.shape}")
+
+    # check enc-dec
+    model=sig2ir_encdec(sig_len=sig_len, l_len=l_len, v_len=v_len, z_len=z_len, ir_len=ir_len)
+    model.to("cpu")
+    model.eval
+    ir=decoder(x_wave)
+    print(f"encdec input shape: {x_wave.shape}")
+    print(f"encdec output shape: {ir.shape}")
 
